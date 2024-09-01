bb30408520e093967f790af7ae1ddb3e219ebc9d
Knarkzel <knarkzel@gmail.com>
2024-09-01 11:53:43 +0200

    day 2

--- src/main.lisp
+++ src/main.lisp
@@ -1,9 +1,9 @@
 ;; Dependencies
-(ql:quickload '(:hunchentoot :easy-routes :spinneret))
+(ql:quickload '(:hunchentoot :easy-routes :spinneret :dexador :com.inuoe.jzon))
 
 (defpackage :app 
-  (:use :cl :easy-routes)
-  (:import-from :spinneret #:with-html-string))
+  (:use :cl :easy-routes :spinneret)
+  (:local-nicknames (:jzon :com.inuoe.jzon)))
 
 (in-package :app)
 
@@ -17,6 +17,10 @@
        (:meta :charset "utf-8")
        (:meta :name "viewport" :content "width=device-width, initial-scale=1")
        (:link :rel "stylesheet" :href "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css")
+       (:link :rel "preconnect" :href "https://fonts.googleapis.com")
+       (:link :rel "preconnect" :href "https://fonts.gstatic.com")
+       (:link :href "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" :rel "stylesheet")
+       (:style (:raw "body { font-family: Roboto; }"))
        (:script :src "https://unpkg.com/htmx.org@2.0.2"))
       (:body :class "container"
        (:h1 ,title)
@@ -24,24 +28,23 @@
 
 ;; Routes
 (defroute home "/" ()
-  (with-page (:title "Welcome")
-    (:p "Welcome to my simple web app!")
-    (:p "Fetch about")
-    (:button :hx-get "/about" :hx-swap "outerHTML")
-    (:a :href "/greet?name=World" "Say hello")))
+  (with-page (:title "Home")
+    (:div
+     (:p "Fetch latest data from news page")
+     (:button :hx-get "/items" :hx-target "div" "Click me"))))
 
-(defroute about "/about" ()
-  (with-page (:title "About")
-    (:p "Who am I?")
-    (:a :href "https://github.com/knarkzel" "More about me")))
-
-(defroute greet "/greet" (name)
-  (with-page (:title "Greeting")
-    (:p (format nil "Hello, ~A!" name))
-    (:a :href "/" "Go back home")))
+(defroute items "/items" ()
+  (let* ((response (dex:get "https://jsonplaceholder.typicode.com/photos"))
+         (items (jzon:parse response)))
+    (with-html-string
+      (:ul
+       (loop for item across items do
+         (:li
+          (:p (gethash "title" item))
+          (:img :src (gethash "url" item))))))))
 
 ;; Create server
-(defvar *server* (make-instance 'easy-routes:easy-routes-acceptor :port 8080))
+(defvar *server* (make-instance 'easy-routes-acceptor :port 8080))
 
 (defun start-server ()
   (hunchentoot:start *server*)
